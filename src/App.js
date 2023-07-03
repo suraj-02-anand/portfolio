8e0b4f05dc7889de950c9b4c83a96103f4e2e351
Suraj <suraj07.anand@gmail.com>
2023-07-03 19:06:08 +0530

    get rid of 404

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
   return (
     <Routes>
       <Route path="/" element={<Index />} />
-      <Route path="*" element={<PageNotFound />} />
+      <Route path="*" element={<Index />} />
     </Routes>
   );
 }
